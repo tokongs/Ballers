6faac5f93103d413bdf089a791cf712c69afe5b5
Jin Tony Nymann <jintony@dhcp-10-22-4-133.wlan.ntnu.no>
2018-10-30 16:46:08 +0100

    Validator for blimedlem og andre oppdateringer til formen

---
+++ script/blimedlem.js
@@ -0,0 +1,19 @@
+function validateForm() {
+    var x = documentForm["bli_medlem"]["navn"].value
+    if (x == "")(
+        alert("Du må fylle ut navn!");
+        return false;
+    )
+
+    var y = documentForm["bli_medlem"]["bruker_mail"].value
+    if (y == "")(
+        alert("Du må fylle ut navn!");
+        return false;
+    )
+
+    var z = documentForm ["bli_medlem"]["lag"].value
+    if (z == "")(
+        alert("Velg et lag")
+        return false;
+    )
+}

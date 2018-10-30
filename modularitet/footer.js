24b44b32fe1e61fca51aae6047c590dbe22c8303
Lars Strømholm <lars.stromholm@gmail.com>
2018-10-30 18:11:56 +0100

    Lagt til footer

--- modularitet/footer.js
+++ modularitet/footer.js
@@ -1,7 +1,7 @@
-//Henter footerelementet fra sidene
-var footer = getElementById("ballersFooter");
+//Lager en variabel som er footerelementet på sidene
+var footer = document.getElementById("ballersFooter");
 
-//Lager et bildeelement til ikonene
+//Lager et bildeelement til hvert av ikonene
 var twitterIcon = document.createElement("img");
 var googleIcon = document.createElement("img");
 var facebookIcon = document.createElement("img");
@@ -18,6 +18,17 @@ twitterIcon.src = "../img/icons/twitterIcon.png";
 googleIcon.src = "../img/icons/googleplusIcon.png";
 facebookIcon.src = "../img/icons/facebookIcon.png";
 snapchatIcon.src = "../img/icons/snapchatIcon.png";
+
+//Legger til link
+//Åpner ny fane når man trykker på ikonet
+twitterLink.href = "https://twitter.com/";
+twitterLink.target = "_blank";
+googleLink.href = "https://plus.google.com/discover";
+googleLink.target = "_blank";
+facebookLink.href = "https://www.facebook.com/";
+facebookLink.target = "_blank";
+snapchatLink.href = "https://www.snapchat.com/";
+snapchatLink.target = "_blank";
 
 //Legger ikonene til linkene
 twitterLink.appendChild(twitterIcon);

15db7a35335f0a0b01c69834617c960efe89c4a2
erikkols <erikako@stud.ntnu.no>
2018-10-30 14:31:25 +0100

    small changes

---
+++ modularitet/popup.js
@@ -0,0 +1,18 @@
+
+
+//document.getElementById("hovedelen").setAttribute("style", "background-color: red;");
+//document.getElementById("infobar").style.height = "800px";
+//var x = document.getElementById("hovedelen");
+
+function popup(clicked_id){    
+    document.getElementById(clicked_id).setAttribute("style", "background-color: red;"+"width:80vw;"+"height:80vh;"+"z-index=1;"+"position:absolute;"
+    +"left:5vw;"+ "top:5vw;");
+    //document.getElementById(clicked_id).setAttribute("style", "width:50%");
+    //document.getElementById(navn).setAttribute("style", "position:fixed");
+    //document.getElementById(navn).setAttribute("style", "z-index:1");
+}
+
+console.log(45+45);
+console.log(aktuell)
+
+

c4a352b3cf90680b3ab4c9061c896912190f1788
erikkols <erikako@stud.ntnu.no>
2018-11-02 14:07:42 +0100

    Fixed hoverfunksjon!

--- modularitet/popup.js
+++ modularitet/popup.js
@@ -2,34 +2,40 @@
 
 
 var diven=Element;                                                                  //initierer diven globalt
-var klar=true;                                                                      //Variabel som sikrer at det bare er en "Popup" artikkel om gangen
+var klar=true;
+const antallArtikkel = document.getElementsByClassName("artikkel");                                                              //Variabel som sikrer at det bare er en "Popup" artikkel om gangen
 
 
 function popup(clicked_id){                             
     if (klar) {
-    diven = document.getElementById(clicked_id);
-    diven.setAttribute("style","width:80vw;"+"height:80vh;"+"z-index=1;"                         //gjør aktuelle diven til stor popup med style.
-    +"position:fixed;"+"left:5vw;"+ "top:5vw;"+"border:solid #f1f1f1 8px;");    
-    
-    document.getElementById("closebutton").setAttribute("style", "display:block;");              //exit-knappen blir synlig
-    diven.getElementsByClassName("imgbox")[0].setAttribute("style","width:30vw;"+"height:60vh;"); //bilde tilhørende aktuell div blir større
-    
-    document.getElementById(clicked_id).classList.remove("hovererBart");
+        diven = document.getElementById(clicked_id);
+        diven.setAttribute("style","width:80vw;"+"height:80vh;"+"z-index=1;"                         //gjør aktuelle diven til stor popup med style.
+        +"position:fixed;"+"left:5vw;"+ "top:5vw;"+"border:solid #f1f1f1 8px;");    
+        
+        document.getElementById("closebutton").setAttribute("style", "display:block;");              //exit-knappen blir synlig
+        diven.getElementsByClassName("imgbox")[0].setAttribute("style","width:30vw;"+"height:60vh;"); //bilde tilhørende aktuell div blir større
+                  
+        for (i=0 ; i<antallArtikkel.length; i++){                                                      //fjerner hoverfunksjonen/class på alle artikkler
+            antallArtikkel[i].className = "artikkel";                                                   
+        }
+            
     }
     klar=false;                                                                                    //gjør klar til false, sånn at popup ikke kjøres før reset er kjørt.
-    console.log(diven)
 }
    
 
 function reset() {                                                                                  //resetter alle forandringene popup.js gjorde
     diven.setAttribute("style", "background-color: default;"+"width:default;"
     +"height:default;"+"z-index=default;"+"position:default;"+"left:default;"+ "top:default;");
+    
     document.getElementById("closebutton").setAttribute("style", "display:none");
 
-    diven.getElementsByClassName("imgbox")[0].setAttribute("style","width:default;"+"height:default;");
+    diven.getElementsByClassName("imgbox")[0].setAttribute("style","width:default;"+"height:default;");  
     
-    diven.classList.add("hovererBart");
-
+    
+    for (i=0 ; i<antallArtikkel.length; i++){                                           //legger til hover funksjonen/class
+        antallArtikkel[i].className = "artikkel hovererBart";
+    }
     klar=true;                                                                                               //gjør klar true, så popup kan kjøres.
 }
 
@@ -37,13 +43,3 @@ function reset() {                                                              
 
 
 
-/*
-artikel_popup
-function fokus(){
-if (klar){    
-   
-    document.querySelector(".artikkel:hover").setAttribute("style","opacity:0.7;"); 
-    console.log("test");
-}
-}
-*/

f1cbd3d3d00d46b7a50bbb0a3798e30f3cf887da
Tobias Kongvik <tokongs98@gmail.com>
2018-11-05 15:16:57 +0100

    merge conflicts

---
+++ script/kontakt.js
@@ -0,0 +1,13 @@
+var diven = document.getElementById("nyDiv");
+document.getElementById("jin").addEventListener("click",hei);
+document.getElementById("mama").addEventListener("click",hei);
+document.getElementById("papa").addEventListener("click",hei);
+
+function hei(){
+  diven.setAttribute("style","height:60%;"+"left:2vw;"+ "bottom:10%;"
+  +"top:20;"+"background-color:#353535;"+"border-style: solid;"+"border-color: #53143B;"+"display:block;");
+}
+
+document.getElementById("cancel").addEventListener("click",function(){
+  diven.setAttribute("style","display:block;")
+})

677990ab04f3702d906ea823452dbd3457b4a967
emilwhj <emilwhj@stud.ntnu.no>
2018-11-06 11:29:26 +0100

    fikset litt

--- script/kontakt.js
+++ script/kontakt.js
@@ -6,8 +6,10 @@ document.getElementById("papa").addEventListener("click",hei);
 function hei(){
   diven.setAttribute("style","height:60%;"+"left:2vw;"+ "bottom:10%;"
   +"top:20;"+"background-color:#353535;"+"border-style: solid;"+"border-color: #53143B;"+"display:block;");
+  document.getElementById("btn").setAttribute("style","display:block;");
 }
 
 document.getElementById("cancel").addEventListener("click",function(){
-  diven.setAttribute("style","display:block;")
+  diven.setAttribute("style","display:none;");
+  document.getElementById("btn").setAttribute("style","display:none;");
 })

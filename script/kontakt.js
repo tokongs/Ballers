975bebe3328401135b96c278fcea80da1c87c772
Tobias Kongvik <tokongs98@gmail.com>
2018-11-07 15:26:46 +0100

    Endringer modularitet og for å gjøre ting emr likt på hver side

--- script/kontakt.js
+++ script/kontakt.js
@@ -1,15 +1,15 @@
-var diven = document.getElementById("nyDiv");
-document.getElementById("jin").addEventListener("click",hei);
-document.getElementById("mama").addEventListener("click",hei);
-document.getElementById("papa").addEventListener("click",hei);
+var popup = document.getElementById("popup");
+document.getElementById("jin").addEventListener("click",aktiverPopup);
+document.getElementById("mama").addEventListener("click",aktiverPopup);
+document.getElementById("papa").addEventListener("click",aktiverPopup);
 
-function hei(){
-  diven.setAttribute("style","height:60%;"+"left:2vw;"+ "bottom:10%;"
+function aktiverPopup(){
+  popup.setAttribute("style","height:60%;"+"left:2vw;"+ "bottom:10%;"
   +"top:20;"+"background-color:#353535;"+"border-style: solid;"+"border-color: #53143B;"+"display:block;");
   document.getElementById("btn").setAttribute("style","display:block;");
 }
 
 document.getElementById("cancel").addEventListener("click",function(){
-  diven.setAttribute("style","display:none;");
+  popup.setAttribute("style","display:none;");
   document.getElementById("btn").setAttribute("style","display:none;");
 })

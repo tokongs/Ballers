8d486b273bc5b76a468b6a3cc9980a0bb4d292f6
Lars Strømholm <lars.stromholm@gmail.com>
2018-11-07 14:54:29 +0100

    Blimedlem-siden javascript

--- script/blimedlem.js
+++ script/blimedlem.js
@@ -1,20 +1,21 @@
-function validateForm() {
-    console.log("hei")
-    var x = document.forms["bli_medlem"]["navn"].value;
-    if (x == "") {
-        alert("Du må fylle ut navn!");
-        return false;
+function validateNavn() {
+    var inputNavn = document.getElementById("navn");
+    if (inputNavn.value == "") {
+      inputNavn.style.borderLeft = "6px solid red";
+      inputNavn.style.backgroundColor = "lightgrey"
+    } else {
+      inputNavn.style.borderLeft = "6px solid green";
+      inputNavn.style.backgroundColor = "white";
     }
+}
 
-    var y = document.forms["bli_medlem"]["bruker_mail"].value;
-    if (y == ""){
-        alert("Du må fylle ut e-post!");
-        return false;
-    }
-
-    var z = document.forms ["bli_medlem"]["lag"].value;
-    if (z == ""){
-        alert("Velg et lag")
-        return false;
+function validateEpost() {
+    var inputEpost = document.getElementById("mail");
+    if (inputEpost.value.includes("@") && inputEpost.value.includes(".")) {
+      inputEpost.style.borderLeft = "6px solid green";
+      inputEpost.style.backgroundColor = "white";
+    } else {
+      inputEpost.style.borderLeft = "6px solid red";
+      inputEpost.style.backgroundColor = "lightgrey"
     }
 }

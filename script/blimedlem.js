6dcde8f5bf456a540ddab63485c6ff09ace89e4d
Lars Strømholm <lars.stromholm@gmail.com>
2018-11-07 13:30:37 +0100

    Fikset blimedlem-siden

--- script/blimedlem.js
+++ script/blimedlem.js
@@ -1,20 +1,20 @@
 function validateForm() {
-    var x = documentForm["bli_medlem"]["navn"].value
-    if (x == "")(
+    console.log("hei")
+    var x = document.forms["bli_medlem"]["navn"].value;
+    if (x == "") {
         alert("Du må fylle ut navn!");
         return false;
-    )
+    }
 
-    var y = documentForm["bli_medlem"]["bruker_mail"].value
-    if (y == "")(
-        alert("Du må fylle ut navn!");
+    var y = document.forms["bli_medlem"]["bruker_mail"].value;
+    if (y == ""){
+        alert("Du må fylle ut e-post!");
         return false;
-    )
+    }
 
-    var z = documentForm ["bli_medlem"]["lag"].value
-    if (z == "")(
+    var z = document.forms ["bli_medlem"]["lag"].value;
+    if (z == ""){
         alert("Velg et lag")
         return false;
-    )
+    }
 }
-    
